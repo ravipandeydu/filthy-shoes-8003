192a01bb57677d1265ef24d57320831e3095d01b
ravipandeydu <rp120896@gmail.com>
2022-08-25 23:31:58 +0530

    Carousel Added

--- rentomojo/src/index.js
+++ rentomojo/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
